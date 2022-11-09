be8555bb83a99eb7c073bbbff4ebc9dee51e2209
jack <jackcheah2@gmail.com>
2022-11-09 21:11:43 +0800

    Move hook to src

--- src/hook/useAuth.js
+++ src/hook/useAuth.js
@@ -1,4 +1,4 @@
-import React, { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState } from "react";
 import http from "../http";
 
 export const authContext = createContext(null);
