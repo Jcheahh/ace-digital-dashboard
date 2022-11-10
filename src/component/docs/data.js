6002fdcf1accd3b0d987581f4f2e44a3f2a3cbdf
jack <jackcheah2@gmail.com>
2022-11-10 21:58:24 +0800

    Some options for filters

---
+++ src/component/docs/data.js
@@ -0,0 +1,34 @@
+export const brandOptions = [
+  { value: "alfa-romero", label: "Alfa-Romero" },
+  { value: "audi", label: "Audi" },
+  { value: "bmw", label: "Bmw" },
+];
+
+export const fuelTypeOptions = [
+  { value: "gas", label: "Gas" },
+  { value: "diesel", label: "Diesel" },
+];
+
+export const aspirationOptions = [
+  { value: "std", label: "Std" },
+  { value: "turbo", label: "Turbo" },
+];
+
+export const numberOfDoorsOptions = [
+  { value: "four", label: "Four" },
+  { value: "two", label: "Two" },
+];
+
+export const bodyStyleOptions = [
+  { value: "convertible", label: "Convertible" },
+  { value: "hatchback", label: "Hatchback" },
+  { value: "sedan", label: "Sedan" },
+  { value: "wagon", label: "Wagon" },
+  { value: "hardtop", label: "Hardtop" },
+];
+
+export const driveWheelsOptions = [
+  { value: "fwd", label: "Fwd" },
+  { value: "rwd", label: "Rwd" },
+  { value: "4wd", label: "4wd" },
+];
