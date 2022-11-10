b63559e2fd18ff44b17888583cafe9444993bce5
jack <jackcheah2@gmail.com>
2022-11-11 00:50:10 +0800

    Finished landing page with tailwind

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -4,20 +4,40 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
   return (
     <>
-      <nav>
-        <Link to="/login">Log In</Link>
-        <Link to="/sign-up">
-          <button type="button">Sign Up</button>
-        </Link>
+      <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-3 px-6 bg-transparent sm:items-baseline w-full container">
+        <p>
+          <Link className={"mr-4 font-semibold leading-normal"} to="/login">
+            Log In
+          </Link>
+          <Link to="/sign-up">
+            <button type="button">Sign Up</button>
+          </Link>
+        </p>
       </nav>
-      <div>
-        <div>
+      <div className="flex justify-center">
+        <div className="text-left max-w-2xl py-14">
           <h1>
-            <span>Dashboard</span>
-            <span>Project</span>
+            <span className="text-6xl text-gray-700 block">
+              Dashboard Project
+            </span>
           </h1>
-          <p>Create / Login your account to view the project</p>
-          <button type="button">It just a button</button>
+          <p className="py-3 text-2xl">
+            <Link to="/sign-up">
+              <button type="button">Create</button>
+            </Link>{" "}
+            /{" "}
+            <Link to="/login">
+              <button type="button">Login</button>
+            </Link>{" "}
+            your account to view the project
+          </p>
+          <button
+            className="ui-button rounded-xl tracking-wide font-semibold  font-display focus:outline-none 
+                              focus:shadow-outline disabled:opacity-50 transform bg-cyan-500 hover:bg-cyan-700 py-4 px-10 text-xl"
+            type="button"
+          >
+            It just a button
+          </button>
         </div>
       </div>
     </>
