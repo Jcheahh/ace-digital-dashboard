945c35716f72e1672ca21580f02aaf8ff8135d41
jack <jackcheah2@gmail.com>
2022-11-11 00:50:24 +0800

    Finished login page with tailwind

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { Form, Field } from "react-final-form";
 import { useAuth } from "../hook/useAuth";
 
@@ -48,45 +48,81 @@ export default function Login() {
   };
   return (
     <>
-      <h1>Log In</h1>
+      <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-3 px-6 bg-transparent sm:items-baseline w-full container">
+        <div className="mb-2 sm:mb-0">
+          <Link to="/" className={["text-xl font-bold"]}>
+            To home
+          </Link>
+        </div>
+      </nav>
+      <div className="mx-auto mt-36 lg:w-1/2 xl:max-w-screen-sm">
+        <div className="mt-10 px-16 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+          <h1 className="text-4xl">Log In</h1>
 
-      <Form
-        onSubmit={handleSubmit}
-        validate={validate}
-        render={({ handleSubmit, submitting }) => (
-          <div className="mt-12">
-            <form onSubmit={handleSubmit} noValidate>
-              <div>Email</div>
-              <Field
-                name="email"
-                render={({ input }) => (
-                  <>
-                    <input {...input} placeholder="example@gmail.com" />
-                  </>
-                )}
-              />
-              <div className="mt-8">
-                <div>Password</div>
-                <Field
-                  name="password"
-                  render={({ input }) => (
-                    <>
-                      <input
-                        {...input}
-                        placeholder="Enter your password"
-                        type="password"
-                      />
-                    </>
-                  )}
-                />
+          <Form
+            onSubmit={handleSubmit}
+            validate={validate}
+            render={({ handleSubmit, submitting }) => (
+              <div className="mt-12">
+                <form onSubmit={handleSubmit} noValidate>
+                  <div className={"text-red-500 text-center mb-4 font-bold"}>
+                    {errMessage}
+                  </div>
+                  <div className={"font-bold text-gray-700 tracking-wide"}>
+                    Email
+                  </div>
+                  <Field
+                    name="email"
+                    render={({ input }) => (
+                      <>
+                        <input
+                          {...input}
+                          className={`w-full text-lg py-2 border-b border-gray-300 focus:outline-none`}
+                          placeholder="example@gmail.com"
+                        />
+                      </>
+                    )}
+                  />
+                  <div className="mt-8">
+                    <div className={"font-bold text-gray-700 tracking-wide"}>
+                      Password
+                    </div>
+                    <Field
+                      name="password"
+                      render={({ input }) => (
+                        <>
+                          <input
+                            {...input}
+                            className={`w-full text-lg py-2 border-b border-gray-300 focus:outline-none`}
+                            placeholder="Enter your password"
+                            type="password"
+                          />
+                        </>
+                      )}
+                    />
+                  </div>
+                  <p
+                    className={[
+                      "flex my-3.5 font-display font-semibold text-sm",
+                    ]}
+                  >
+                    Don&apos;t have an account?&nbsp;{" "}
+                    <Link to="/sign-up">Sign up</Link>
+                  </p>
+                  <button
+                    className="ui-button rounded-xl tracking-wide font-semibold  font-display focus:outline-none 
+                              focus:shadow-outline disabled:opacity-50 transform bg-cyan-500 hover:bg-cyan-700 py-1.5 px-4"
+                    type="submit"
+                    disabled={submitting}
+                  >
+                    {submitting ? "Loading..." : "Log In"}
+                  </button>
+                </form>
               </div>
-              <button type="submit" disabled={submitting}>
-                Log in
-              </button>
-            </form>
-          </div>
-        )}
-      />
+            )}
+          />
+        </div>
+      </div>
     </>
   );
 }
