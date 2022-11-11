488f651a2a9dd81f66c1cf4c20d552b4c7e607f9
jack <jackcheah2@gmail.com>
2022-11-11 12:21:07 +0800

    Finished System page UI with tailwind and css

--- src/component/System.js
+++ src/component/System.js
@@ -12,6 +12,7 @@ import {
 } from "./docs/data";
 import { Pie, Bar, Line } from "react-chartjs-2";
 import Chart from "chart.js/auto";
+import "./System.css";
 
 export default function System() {
   const initialFilter = {
@@ -23,26 +24,63 @@ export default function System() {
     drive_wheels: [],
   };
 
-  const sstate = {
-    labels: ["January", "February", "March", "April", "May"],
+  const [cars, setCars] = useState([]);
+  const [toFilters, setToFilters] = useState(initialFilter);
+
+  const animatedComponents = makeAnimated();
+
+  const fetchCars = () => {
+    http.get(`/api/cars`).then((body) => {
+      setCars(body.data);
+    });
+  };
+
+  const reducedCar = Object.entries(
+    cars.reduce((state, car) => {
+      if (state[car.brand]) {
+        state[car.brand]++;
+      } else {
+        state[car.brand] = 1;
+      }
+      return state;
+    }, {})
+  );
+
+  useEffect(() => {
+    fetchCars();
+  }, []);
+
+  const barState = {
+    labels: reducedCar.map(([brand, _]) => brand),
     datasets: [
       {
-        label: "Rainfall",
-        fill: false,
-        lineTension: 0.4,
+        label: "Car Sold Amount",
         backgroundColor: "rgba(75,192,192,1)",
         borderColor: "rgba(0,0,0,1)",
         borderWidth: 2,
-        data: [65, 59, 80, 81, 56],
+        data: reducedCar.map(([_, amount]) => amount),
       },
     ],
   };
-
-  const ssstate = {
-    labels: ["January", "February", "March", "April", "May"],
+  const lineState = {
+    labels: reducedCar.map(([brand, _]) => brand),
     datasets: [
       {
-        label: "Rainfall",
+        label: "Car Sold Amount",
+        lineTension: 0.5,
+        fill: false,
+        backgroundColor: "rgba(75,192,192,1)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: reducedCar.map(([_, amount]) => amount),
+      },
+    ],
+  };
+  const pieState = {
+    labels: reducedCar.map(([brand, _]) => brand),
+    datasets: [
+      {
+        label: "Car Sold Amount",
         backgroundColor: [
           "#B21F00",
           "#C9DE00",
@@ -57,46 +95,10 @@ export default function System() {
           "#003350",
           "#35014F",
         ],
-        data: [65, 59, 80, 81, 56],
-      },
-    ],
-  };
-
-  const [cars, setCars] = useState([]);
-  const [toFilters, setToFilters] = useState(initialFilter);
-
-  const animatedComponents = makeAnimated();
-  const fetchCars = () => {
-    http.get(`/api/cars`).then((body) => {
-      setCars(body.data);
-    });
-  };
-
-  useEffect(() => {
-    console.log(xxx[0]);
-    fetchCars();
-  }, []);
-
-  const xxx = Object.entries(
-    cars.reduce((state, car) => {
-      if (state[car.brand]) {
-        state[car.brand]++;
-      } else {
-        state[car.brand] = 1;
-      }
-      return state;
-    }, {})
-  ).sort(([_, a], [__, b]) => b - a);
-
-  const state = {
-    labels: ["Toyota", "Nissan", "Mercedes-Benz", "Bmw", "Honda"],
-    datasets: [
-      {
-        label: "Car Brand",
-        backgroundColor: "rgba(75,192,192,1)",
         borderColor: "rgba(0,0,0,1)",
+
         borderWidth: 2,
-        data: [65, 59, 80, 81, 56],
+        data: reducedCar.map(([_, amount]) => amount),
       },
     ],
   };
@@ -125,120 +127,147 @@ export default function System() {
   };
   return (
     <>
-      <div>
-        <div>
-          <span>Brand</span>
-          <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            defaultValue={[brandOptions[0]]}
-            isMulti
-            options={brandOptions}
-            onChange={(choice) => filter("brand", choice)}
-            value={toFilters.brand}
-          />
-          <span>Fuel Type</span>
-          <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            isMulti
-            options={fuelTypeOptions}
-            onChange={(choice) => filter("fuel_type", choice)}
-            value={toFilters.fuel_type}
-          />
-          <span>Aspiration</span>
-          <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            isMulti
-            options={aspirationOptions}
-            onChange={(choice) => filter("aspiration", choice)}
-            value={toFilters.aspiration}
-          />
-          <span>Number of doors</span>
-          <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            isMulti
-            options={numberOfDoorsOptions}
-            onChange={(choice) => filter("num_of_doors", choice)}
-            value={toFilters.num_of_doors}
-          />
-          <span>Body style</span>
-          <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            isMulti
-            options={bodyStyleOptions}
-            onChange={(choice) => filter("body_style", choice)}
-            value={toFilters.body_style}
-          />
-          <span>Drive Wheels</span>
-          <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            isMulti
-            options={driveWheelsOptions}
-            onChange={(choice) => filter("drive_wheels", choice)}
-            value={toFilters.drive_wheels}
-          />
+      <div className="flex flex-row">
+        <aside className="w-64" aria-label="Sidebar">
+          <div className="overflow-y-auto py-6 px-5 bg-gray-50 rounded h-full">
+            <span>Brand</span>
+            <Select
+              closeMenuOnSelect={false}
+              components={animatedComponents}
+              defaultValue={[brandOptions[0]]}
+              isMulti
+              options={brandOptions}
+              onChange={(choice) => filter("brand", choice)}
+              value={toFilters.brand}
+            />
+            <span>Fuel Type</span>
+            <Select
+              closeMenuOnSelect={false}
+              components={animatedComponents}
+              isMulti
+              options={fuelTypeOptions}
+              onChange={(choice) => filter("fuel_type", choice)}
+              value={toFilters.fuel_type}
+            />
+            <span>Aspiration</span>
+            <Select
+              closeMenuOnSelect={false}
+              components={animatedComponents}
+              isMulti
+              options={aspirationOptions}
+              onChange={(choice) => filter("aspiration", choice)}
+              value={toFilters.aspiration}
+            />
+            <span>Number of doors</span>
+            <Select
+              closeMenuOnSelect={false}
+              components={animatedComponents}
+              isMulti
+              options={numberOfDoorsOptions}
+              onChange={(choice) => filter("num_of_doors", choice)}
+              value={toFilters.num_of_doors}
+            />
+            <span>Body style</span>
+            <Select
+              closeMenuOnSelect={false}
+              components={animatedComponents}
+              isMulti
+              options={bodyStyleOptions}
+              onChange={(choice) => filter("body_style", choice)}
+              value={toFilters.body_style}
+            />
+            <span>Drive Wheels</span>
+            <Select
+              closeMenuOnSelect={false}
+              components={animatedComponents}
+              isMulti
+              options={driveWheelsOptions}
+              onChange={(choice) => filter("drive_wheels", choice)}
+              value={toFilters.drive_wheels}
+            />
+            <div className="pt-2">
+              <button type="button" className="pl-5" onClick={reset}>
+                Reset
+              </button>
+              <button type="button" className="pl-5" onClick={applyFilter}>
+                Apply
+              </button>
+            </div>
+          </div>
+        </aside>
+
+        <div className="pl-5">
+          <div className="space-y-8 md:space-y-0 md:space-x-8 lg:flex xl:items-center">
+            <div>
+              <p className="chart">
+                <Bar
+                  data={barState}
+                  options={{
+                    title: {
+                      display: true,
+                      text: "Average Rainfall per month",
+                      fontSize: 20,
+                    },
+                    legend: {
+                      display: true,
+                      position: "right",
+                    },
+                  }}
+                />
+              </p>
+            </div>
+            <div>
+              <p className="chart">
+                <Line
+                  data={lineState}
+                  options={{
+                    title: {
+                      display: true,
+                      text: "Average Rainfall per month",
+                      fontSize: 20,
+                    },
+                    legend: {
+                      display: true,
+                      position: "right",
+                    },
+                  }}
+                />
+              </p>
+            </div>
+            <div>
+              <p className="chart">
+                <Pie
+                  data={pieState}
+                  options={{
+                    title: {
+                      display: true,
+                      text: "Average Rainfall per month",
+                      fontSize: 20,
+                    },
+                    legend: {
+                      display: true,
+                      position: "right",
+                    },
+                  }}
+                />
+              </p>
+            </div>
+          </div>
+          <div className="pt-8">
+            <table>
+              <tr>
+                <th className="pr-8">Car Sold Brand</th>
+                <th>Total</th>
+              </tr>
+              {reducedCar.map(([brand, amount]) => (
+                <tr>
+                  <td>{brand}</td>
+                  <td>{amount}</td>
+                </tr>
+              ))}
+            </table>
+          </div>
         </div>
-        <button type="button" onClick={reset}>
-          Reset
-        </button>
-        <button type="button" onClick={applyFilter}>
-          Apply
-        </button>
-      </div>
-      <div>
-        <h1>
-          <span>Dashboard</span>
-          <span>Project</span>
-        </h1>
-        <p style={{ width: 700 }}>
-          <Bar
-            data={state}
-            options={{
-              title: {
-                display: true,
-                text: "Average Rainfall per month",
-                fontSize: 20,
-              },
-              legend: {
-                display: true,
-                position: "right",
-              },
-            }}
-          />
-          <Line
-            data={sstate}
-            options={{
-              title: {
-                display: true,
-                text: "Average Rainfall per month",
-                fontSize: 20,
-              },
-              legend: {
-                display: true,
-                position: "right",
-              },
-            }}
-          />
-          <Pie
-            data={ssstate}
-            options={{
-              title: {
-                display: true,
-                text: "Average Rainfall per month",
-                fontSize: 20,
-              },
-              legend: {
-                display: true,
-                position: "right",
-              },
-            }}
-          />
-        </p>
       </div>
     </>
   );
